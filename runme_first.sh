206c8f96dac5028e7c5fc149d6a18596267c5a92
mainframed <mainframed767@gmail.com>
2023-08-01 22:00:33 -0700

    updating the runme file

--- runme_first.sh
+++ runme_first.sh
@@ -110,10 +110,10 @@ wget -O ~/Labs/dummy_client.py "https://raw.githubusercontent.com/mainframed/DEF
 ARCH=$(dpkg --print-architecture)
 if [ "$ARCH" = "amd64" ]; then
     wget -O ~/Labs/client "https://github.com/mainframed/DEFCON31/raw/main/client_amd64" 
-    docker run -d --name defcon31 -p 127.0.0.1:1234:1234 -p 127.0.0.1:31337:31337 mainframed767/defcon31:amd64
+    docker run -d --name defcon31 -p 127.0.0.1:1234:1234 -p 127.0.0.1:31337:31337 -p 127.0.0.1:8080:8080 mainframed767/defcon31:amd64
 elif [ "$ARCH" = "arm64" ]; then
     wget -O ~/Labs/client "https://github.com/mainframed/DEFCON31/raw/main/client_arm64" 
-    docker run -d --name defcon31 -p 127.0.0.1:1234:1234 -p 127.0.0.1:31337:31337 mainframed767/defcon31:arm64
+    docker run -d --name defcon31 -p 127.0.0.1:1234:1234 -p 127.0.0.1:31337:31337 -p 127.0.0.1:8080:8080 mainframed767/defcon31:arm64
 else
     echo "${RED}ERROR ERROR ERROR"
     echo " Talk to the instructor about this error"
